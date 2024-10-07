da67cace8351117e39766fc5aad4729b91199e74
diyaammianickal <diya.ammianickal@collaboration-factory.de>
2024-10-07 13:22:00 +0200

    added triggers.

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {CarouselModule} from "primeng/carousel";
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
   ]
 })
 export class ProductsModule { }
