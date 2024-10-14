379c2eef4b0af23ab82c5b047b4859a68be088e4
diyaammianickal <diya.ammianickal@collaboration-factory.de>
2024-10-14 17:09:16 +0200

    Added control flow, standalone example.

---
+++ src/app/products/limited-products/limited-products.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-limited-products',
+  standalone: false,
+  templateUrl: './limited-products.component.html',
+  styleUrl: './limited-products.component.scss'
+})
+export class LimitedProductsComponent {
+
+}
