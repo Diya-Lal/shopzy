6a1f892ea251157e316bce77dd99421e2f9a3499
eduinaalicka <eduina.alicka@gmail.com>
2024-10-21 22:16:21 +0200

    signals

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -9,12 +9,36 @@ import {Product} from "../models/product.model";
 
 export class ProductsService {
 
-  ROOT_URL ="http://localhost:9002/api";
-  http = inject(HttpClient);
+  private readonly ROOT_URL ="http://localhost:9002/api";
+  private readonly http = inject(HttpClient);
 
+  // loads ALL prducts (R)
   public async loadAllProducts():Promise<Product[]> {
     const $products = this.http.get<Product[]>(`${this.ROOT_URL}/products`); // load it as an observable
     return firstValueFrom($products); // convert it to a promise of type Product[]
   }
 
+  // loads a SINGLE product by ID (R)
+  public async loadProductById(id: string):Promise<Product> {
+    const $product = this.http.get<Product>(`${this.ROOT_URL}/products/${id}`);
+    return firstValueFrom($product);
+  }
+
+  // create a new product (C)
+  public async createProduct(product: Product):Promise<Product> {
+    const $newProduct = this.http.post<Product>(`${this.ROOT_URL}/products`, product);
+    return firstValueFrom($newProduct);
+  }
+
+  // update a product (U)
+  public async editProduct(id: number, product: Product):Promise<Product> {
+    const $updatedProduct = this.http.put<Product>(`${this.ROOT_URL}/products/${id}`, product);
+    return firstValueFrom($updatedProduct);
+  }
+
+  // delete a product by ID (D)
+  public async deleteProduct(id: number):Promise<void> {
+    const $deleteResponse = this.http.delete<void>(`${this.ROOT_URL}/products/${id}`);
+    return firstValueFrom($deleteResponse);
+  }
 }
